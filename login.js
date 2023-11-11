981393a3839710419260751e25ef9c441717b486
gsr1219676846 <gsr1219676846@gmail.com>
2023-11-11 11:18:36 +1000

    version 0.2

--- login.js
+++ login.js
@@ -1,12 +1,17 @@
 
-// 打開登錄表單懸浮層
-function openLoginForm() {
-    document.getElementById('loginForm').style.display = 'block';
-}
-
-// 關閉登錄表單懸浮層
-function closeLoginForm() {
-    document.getElementById('loginForm').style.display = 'none';
-}
-
-dsufsfhuidshfish
+// 在頁面加載完成後執行的函數
+window.onload = function() {
+    // 確保登錄表單是隱藏的
+    closeLoginForm();
+  };
+  
+  // 打開登錄表單懸浮層
+  function openLoginForm() {
+      document.getElementById('loginForm').style.display = 'block';
+  }
+  
+  // 關閉登錄表單懸浮層
+  function closeLoginForm() {
+      document.getElementById('loginForm').style.display = 'none';
+  }
+  
