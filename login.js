926966b9e14dca5ca3c87a3fa3a2f1d43a591801
gsr1219676846 <gsr1219676846@gmail.com>
2023-11-11 10:17:26 +1000

    with a login system

---
+++ login.js
@@ -0,0 +1,11 @@
+
+// 打開登錄表單懸浮層
+function openLoginForm() {
+    document.getElementById('loginForm').style.display = 'block';
+}
+
+// 關閉登錄表單懸浮層
+function closeLoginForm() {
+    document.getElementById('loginForm').style.display = 'none';
+}
+
