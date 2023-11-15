f9cc8c00066dd98477990928e155db8a98b0c972
gsr1219676846 <gsr1219676846@gmail.com>
2023-11-15 19:00:49 +1000

    update login system

--- login.js
+++ login.js
@@ -1,16 +1,16 @@
 
-// 在頁面加載完成後執行的函數
+// Function executed after the page is loaded
 window.onload = function() {
-    // 確保登錄表單是隱藏的
+    // Make sure the login form is hidden
     closeLoginForm();
   };
   
-  // 打開登錄表單懸浮層
+  // Open the login form floating layer
   function openLoginForm() {
       document.getElementById('loginForm').style.display = 'block';
   }
   
-  // 關閉登錄表單懸浮層
+  // Close the login form floating layer
   function closeLoginForm() {
       document.getElementById('loginForm').style.display = 'none';
   }
