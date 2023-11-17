37d4ae0eadb301d14945c9bd885e5c7ad30a88e2
gsr1219676846 <gsr1219676846@gmail.com>
2023-11-17 15:21:13 +1000

    login form update

--- login.js
+++ login.js
@@ -15,3 +15,27 @@ window.onload = function() {
       document.getElementById('loginForm').style.display = 'none';
   }
   
+
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  var xhr = new XMLHttpRequest();
+  var url = "http://localhost:8080/website_login/index.php"; // Please make sure this is the correct path to the PHP script that handles login
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.getElementById("loginResult").innerHTML = xhr.responseText;
+    }
+  };
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var data = "email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);
+  xhr.send(data);
+});
+
+
+
