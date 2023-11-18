d6f5d8091af4bc9df70649aa5a0752c1166b9a6b
gsr1219676846 <62987318+gsr1219676846@users.noreply.github.com>
2023-11-18 10:58:42 +1000

    login form

--- login.js
+++ login.js
@@ -1,41 +1,67 @@
 
 // Function executed after the page is loaded
 window.onload = function() {
-    // Make sure the login form is hidden
-    closeLoginForm();
-  };
-  
-  // Open the login form floating layer
-  function openLoginForm() {
-      document.getElementById('loginForm').style.display = 'block';
-  }
-  
-  // Close the login form floating layer
-  function closeLoginForm() {
-      document.getElementById('loginForm').style.display = 'none';
-  }
-  
+  // Make sure the login form is hidden
+  closeLoginForm();
 
+  // Check user logged in status
+  checkUserLoggedIn();
 
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-  e.preventDefault();
+  // Event listener for login form submission
+  document.getElementById('loginForm').addEventListener('submit', function(e) {
+      e.preventDefault();
 
+      var xhr = new XMLHttpRequest();
+      var url = "http://localhost:8080/website_login/loginHandler.php"; // Update this path if needed
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+              document.getElementById("loginResult").innerHTML = xhr.responseText;
+              // Re-check login status after login attempt
+              checkUserLoggedIn();
+          }
+      };
+
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+      var data = "email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);
+      xhr.send(data);
+  });
+
+  // Event listener for account button click
+  document.getElementById('accountButton').addEventListener('click', function() {
+      var dropdown = document.getElementById('accountDropdown');
+      dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
+  });
+};
+
+// Open the login form floating layer
+function openLoginForm() {
+  document.getElementById('loginFormContainer').style.display = 'block';
+}
+
+// Close the login form floating layer
+function closeLoginForm() {
+  document.getElementById('loginFormContainer').style.display = 'none';
+}
+
+// Function to check user login status
+function checkUserLoggedIn() {
   var xhr = new XMLHttpRequest();
-  var url = "http://localhost:8080/website_login/index.php"; // Please make sure this is the correct path to the PHP script that handles login
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      document.getElementById("loginResult").innerHTML = xhr.responseText;
-    }
+  xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          console.log("Response from check_login_status.php: " + this.responseText); // Debug output
+          if (this.responseText === "loggedIn") {
+              document.getElementById('loginButton').style.display = 'none';
+              document.getElementById('accountButton').style.display = 'block';
+          } else {
+              document.getElementById('loginButton').style.display = 'block';
+              document.getElementById('accountButton').style.display = 'none';
+          }
+      }
   };
-
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var data = "email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);
-  xhr.send(data);
-});
-
-
-
+  xhr.open("GET", "http://localhost:8080/website_login/check_login_status.php", true); // Update this path if needed
+  xhr.send();
+}
