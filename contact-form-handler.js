3733db7149f130675980c113547c2231549552c5
Urim Shkreli <urim.shkreli@gmail.com>
2023-11-18 16:19:28 +1000

    18.11.2023 4:20pm Urim update

--- contact-form-handler.js
+++ contact-form-handler.js
@@ -7,3 +7,18 @@ function submitForm(form) {
     // Prevent the default form submission
     return false;
 }
+
+// Services Filter
+function filterServices() {
+    var category = document.getElementById("serviceFilter").value;
+    var services = document.getElementsByClassName("service");
+    
+    for (var i = 0; i < services.length; i++) {
+      if (category == "all" || services[i].dataset.category == category) {
+        services[i].style.display = "";
+      } else {
+        services[i].style.display = "none";
+      }
+    }
+  }
+  
--- login.js
+++ login.js
@@ -8,27 +8,7 @@ window.onload = function() {
   checkUserLoggedIn();
 
   // Event listener for login form submission
-  document.getElementById('loginForm').addEventListener('submit', function(e) {
-      e.preventDefault();
-
-      var xhr = new XMLHttpRequest();
-      var url = "http://localhost:8080/website_login/loginHandler.php"; // Update this path if needed
-      xhr.open("POST", url, true);
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-      xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4 && xhr.status === 200) {
-              document.getElementById("loginResult").innerHTML = xhr.responseText;
-              // Re-check login status after login attempt
-              checkUserLoggedIn();
-          }
-      };
-
-      var email = document.getElementById("email").value;
-      var password = document.getElementById("password").value;
-      var data = "email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);
-      xhr.send(data);
-  });
+  
 
   // Event listener for account button click
   document.getElementById('accountButton').addEventListener('click', function() {
@@ -47,21 +27,3 @@ function closeLoginForm() {
   document.getElementById('loginFormContainer').style.display = 'none';
 }
 
-// Function to check user login status
-function checkUserLoggedIn() {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          console.log("Response from check_login_status.php: " + this.responseText); // Debug output
-          if (this.responseText === "loggedIn") {
-              document.getElementById('loginButton').style.display = 'none';
-              document.getElementById('accountButton').style.display = 'block';
-          } else {
-              document.getElementById('loginButton').style.display = 'block';
-              document.getElementById('accountButton').style.display = 'none';
-          }
-      }
-  };
-  xhr.open("GET", "http://localhost:8080/website_login/check_login_status.php", true); // Update this path if needed
-  xhr.send();
-}
